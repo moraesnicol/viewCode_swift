979e8f9064d71e3018f40d28e3f3c53198d7e451
Gabriel Nicol <moraesnicol@gmail.com>
2021-06-18 04:15:30 -0300

    view code setup ok

--- viewCode_swift/ViewController.swift
+++ viewCode_swift/ViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .systemGreen
+        self.view = view
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
